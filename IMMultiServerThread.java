eff31d24e094d92d57ad2456981a49022e59a2fb
p-deb <45210221+p-deb@users.noreply.github.com>
2019-05-14 17:51:12 -0400

    Update IMMultiServerThread.java

--- IMMultiServerThread.java
+++ IMMultiServerThread.java
@@ -1,11 +1,11 @@
 import java.net.*;
 import java.io.*;
 
-public class IMMultiServerThread extends Thread {
+public class MultiServerThread extends Thread {
     private Socket socket = null;
 
-    public IMMultiServerThread(Socket socket) {
-        super("IMMultiServerThread");
+    public MultiServerThread(Socket socket) {
+        super("MultiServerThread");
         this.socket = socket;
     }
 
